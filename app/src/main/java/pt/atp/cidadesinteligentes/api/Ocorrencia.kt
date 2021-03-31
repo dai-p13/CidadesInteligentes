3f1583ba723c6a5ecbfb5480fa24f95e340d6c47
dai-p13 <ds2000117@gmail.com>
2021-03-31 15:52:49 +0100

    retrofit dependencies

---
+++ app/src/main/java/pt/atp/cidadesinteligentes/api/Ocorrencia.kt
@@ -0,0 +1,12 @@
+package pt.atp.cidadesinteligentes.api
+
+data class Ocorrencia(
+        val id: Int?,
+        val titulo: String,
+        val descricao: String,
+        val latitude: String,
+        val longitude: String,
+        val foto: String,
+        val users_id: Int,
+        val tipo_id: Int
+)
---
+++ app/src/main/java/pt/atp/cidadesinteligentes/api/User.kt
@@ -0,0 +1,7 @@
+package pt.atp.cidadesinteligentes.api
+
+data class User (
+        val id: Int,
+        val username: String,
+        val password: String
+)
