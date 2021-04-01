fe3c6a2e2ad1a8bea1f02b724efa43cbdbe656df
dai-p13 <ds2000117@gmail.com>
2021-04-01 11:01:31 +0100

    commit

--- app/src/main/java/pt/atp/cidadesinteligentes/MapsActivity.kt
+++ app/src/main/java/pt/atp/cidadesinteligentes/MapsActivity.kt
@@ -51,7 +51,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         })
 
     }
-
+    //
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
