731f702dce8c3dd091f092eaf702787bb52ffea7
dai-p13 <ds2000117@gmail.com>
2021-04-06 17:25:22 +0100

    shared prefences? maybe

--- app/src/main/java/pt/atp/cidadesinteligentes/MainActivity.kt
+++ app/src/main/java/pt/atp/cidadesinteligentes/MainActivity.kt
@@ -1,17 +1,16 @@
 package pt.atp.cidadesinteligentes
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import pt.atp.cidadesinteligentes.api.EndPoints
-import pt.atp.cidadesinteligentes.api.Ocorrencia
 import pt.atp.cidadesinteligentes.api.ServiceBuilder
 import pt.atp.cidadesinteligentes.api.Users
 import retrofit2.Call
@@ -23,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var user: EditText
     private lateinit var pwd: EditText
     private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var util: List<Users>
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,21 +37,25 @@ class MainActivity : AppCompatActivity() {
 
         val button2 = findViewById<Button>(R.id.buttonLogin)
         button2.setOnClickListener{
-            val intent = Intent(this@MainActivity, MapsActivity::class.java)
-            startActivity(intent)
-            //realizarLogin()
+            //val intent = Intent(this@MainActivity, MapsActivity::class.java)
+            //startActivity(intent)
+            realizarLogin()
             finish()
         }
 
         user = findViewById(R.id.username)
         pwd = findViewById(R.id.password)
-        //sharedPreferences = getSharedPreferences(getString(R.string.user_creds_file_key), Context.MODE_PRIVATE)
+        sharedPreferences = getSharedPreferences(getString(R.string.share_preferencees_file), Context.MODE_PRIVATE)
 
-        //val nomeUser = sharedPreferences.getString(getString(R.string.username), "")
-        /*val pwd = sharedPreferences.getString(getString(R.string.password), "")
+        val nomeUser = sharedPreferences.getString(getString(R.string.username), "")
+        val pwd = sharedPreferences.getString(getString(R.string.password), "")
         //
         if(nomeUser.toString().isNotEmpty() && pwd.toString().isNotEmpty()) {
-        }*/
+            val intent = Intent(this@MainActivity, MapsActivity::class.java)
+            startActivity(intent)
+        } else {
+            realizarLogin()
+        }
     }
 
     fun realizarLogin() {
@@ -61,12 +65,27 @@ class MainActivity : AppCompatActivity() {
             val request = ServiceBuilder.buildService(EndPoints::class.java)
             val call = request.getUsers()
             call.enqueue(object : Callback<List<Users>> {
+                @SuppressLint("ResourceType")
                 override fun onResponse(call: Call<List<Users>>, response: Response<List<Users>>) {
                     if(response.isSuccessful){
+                        util = response.body()!!
+                        for(utili in util){
+                            val usn = utili.username
+                            val psw = utili.password
+                            val shrePref: SharedPreferences = getSharedPreferences(getString(R.string.share_preferencees_file), Context.MODE_PRIVATE)
+                            with(shrePref.edit()) {
+                                putString(getString(R.id.username), usn)
+                                putString(getString(R.id.password), psw)
+                                commit()
+                            }
+                            Log.d("pref", shrePref.toString())
+                        }
                         val intent = Intent(this@MainActivity, MapsActivity::class.java)
                         startActivity(intent)
                     }else {
-                        Toast.makeText(this@MainActivity, getString(R.string.blank), Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this@MainActivity, getString(R.string.error), Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this@MainActivity, MainActivity::class.java)
+                        startActivity(intent)
                     }
                 }
 
@@ -75,7 +94,7 @@ class MainActivity : AppCompatActivity() {
                 }
             })
         }else {
-            Toast.makeText(this@MainActivity, getString(R.string.error), Toast.LENGTH_SHORT).show()
+            Toast.makeText(this@MainActivity, getString(R.string.blank), Toast.LENGTH_SHORT).show()
         }
     }
 
